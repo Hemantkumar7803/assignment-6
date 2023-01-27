fc18b2f5cf1429d2d9eb1025864388362e2958e6
Hemantkumar7803 <121182169+Hemantkumar7803@users.noreply.github.com>
2023-01-27 23:10:52 +0530

    Update Assignment 6 Q8.py

--- Assignment 6 Q8.py
+++ Assignment 6 Q8.py
@@ -1,4 +1,4 @@
 # assignment-6
 def student_data(student_id, student_name, student_class):
     return f'Student ID: {student_id}\nStudent Name: {student_name}\nClass: {student_class}'
-print(student_data('22105084', ' Saksham gumber', 'ECE'))
+print(student_data('22105106', ' Hemant Kumar', 'ECE'))
